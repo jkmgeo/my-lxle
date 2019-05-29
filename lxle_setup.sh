f3a7fdfb4a6cb5a2811fcc81d8ae1b921aebd3ef
J.K. Muller <jkm02009@gmail.com>
2019-05-29 09:56:20 -0400

    update lxle_setup.sh with conda/snap installs; formatting

--- lxle_setup.sh
+++ lxle_setup.sh
@@ -1,12 +1,19 @@
+# sudo apt install pkg vs sudo apt-get install pkg?
+########
 # install LXLE 16.04 Eclectica
+########
+#
 # Appearance:
+#
 sudo add-apt-repository ppa:noobslab/macbuntu
 sudo apt-get update
 sudo apt-get install macbuntu-os-icons-lts-v7
 sudo apt-get install plank
 sudo apt-get install macbuntu-os-plank-theme-lts-v7
 sudo apt-get install macbuntu-os-ithemes-lts-v7
+#
 # other GUI settings:
+#
 # download wallpaper
 # mogrify -format png ~/Downloads/wallpaper.jpg
 # sudo cp ~/Downloads/wallpaper.png /usr/local/bin/grub.png
@@ -15,55 +22,116 @@ sudo apt-get install macbuntu-os-ithemes-lts-v7
 # (default /usr/local/bin/compasses_set-wallpaper-1680x1050.jpg)
 # sudo nano /boot/grub/grub.cfg > ctrl+w "background_image"
 # sudo cp ~/Downloads/grub.png /etc/grub.d/backgrounds/grub.png
+#
 # Bluetooth:
+#
 # xinput --list --short
 # xinput --set-prop <id> "Device Accel Constant Deceleration" 3-4 (low=fast, vv)
 # reset w/ <
 # xinput --set-prop :id: "Device Accel Velocity Scaling" 1
 # xinput --set-prop :id: "Device Accel Profile" -1
 # >
+#
 # albert (Spotlight):
+#
 sudo apt-get install albert
+
+########
 #Firefox:
+########
 #curl -o /home/jimmy/firefox.tar.bz2 https://download.mozilla.org/?product-firefox-latest-ssl&os=linux64&lang=en-US
 #cd ~
 #tar xjf firefox.tar.bz2
+
+########
 # Miniconda:
+########
 cd ~/Downloads
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-# Terminal:
+#
+# Terminal settings:
+#
 echo 'export PATH="/home/jimmy/miniconda3/bin:$PATH"' >> ~/.bash_profile
 echo 'alias o="xdg-open"' >> ~/.bash_profile
 source ~/.bash_profile
+#
+# Conda installs
+#
+conda install -y numpy
+conda install -y pandas
+conda install -y matplotlib
+#conda install -y plotly_express
+conda install -y scipy
+conda install -y spyder
+# also sudo apt install spyder
+conda install -y jupyter
+conda install -y geopandas
+conda install -y gdal
+# also `descartes` `fiona` `shapely` etc.
+conda install -y terminalplot
+
+########
+# VS Code
+########
+# install snap if needed:
+#sudo apt install snapd
+# or, install as snap application from `snapcraft.io`
+sudo snap install --classic code
+
+########
 # Spotify:
+########
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install spotify-client
+
+########
 # Cisco VPN:
+########
 sudo apt install network-manager-openconnect
 sudo systemctl daemon-reload
 #~/firefox/firefox http://vpn.fas.harvard.edu
+
+########
 # Google Drive:
+########
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install grive
-mkdir -p ~/grive
-cd ~/grive
-grive -as /Harvard/Research/Data
+#mkdir -p ~/grive
+#cd ~/grive
+#grive -as /Harvard/Research/Data
+
+########
 # Dropbox
+########
 # dropbox.com/download?dl=packages/ubuntu/dropbox_yyyy.mm.dd_amd64.db
 # software center
+
+########
 # QGIS (failed LXLE, pass Lubuntu 18.10):
+########
 # sudo sh -c 'echo "deb https://qgis.org/ubuntu-ltr xenial main" >> /etc/apt/sources.list'
 # sudo sh -c 'echo "deb-src https://qgis.org/ubuntu-ltr xenial main" >> /etc/apt/sources.list'
 # sudo apt-get update
 # sudo apt-get install qgis python-qgis qgis-plugin-grass
+
+########
 # Lubuntu Software Center
+########
+#
 # App removal:
+#
 # pidgin arista biniax samba granule Gnome_breakout Gnujump Gweled Linphone
 # Mahjongg Mastermind  Parole_media_player Zaz
 # sudo apt-get remove --purge seamonkey-mozilla-build (other seamonkey too?)
+#
 # App install:
-# firefox vlc 
+#
+# firefox vlc
+#
+########
+# 
+########
